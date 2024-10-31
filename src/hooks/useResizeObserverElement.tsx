0f62f44e49941455a824fdd2186a6778c9f5c96d
njcushing <129661299+njcushing@users.noreply.github.com>
2024-10-31 16:26:00 +0000

    Copy 'useResizeObserverElement' hook from another project

---
+++ src/hooks/useResizeObserverElement.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import * as calculateElementSize from "@/utils/calculateElementSize";
+
+type Params = {
+    ref: React.MutableRefObject<HTMLElement | null>;
+};
+
+type ReturnTypes = [[number, number], React.Dispatch<React.SetStateAction<[number, number]>>];
+
+function useResizeObserverElement(
+    params: Params,
+    dependencies: unknown[] | undefined = undefined,
+): ReturnTypes {
+    const [elementSize, setElementSize] = useState<[number, number]>([0, 0]);
+
+    const { ref } = params;
+
+    useEffect(() => {
+        let refCurrent: Element;
+        const observer = new ResizeObserver((entries) => {
+            setElementSize(calculateElementSize.fromResizeObserverEntry(entries[0]));
+        });
+        if (ref.current) {
+            refCurrent = ref.current;
+            observer.observe(ref.current);
+        }
+        return () => {
+            if (refCurrent instanceof Element) observer.unobserve(refCurrent);
+        };
+    }, [ref, dependencies]);
+
+    return [elementSize, setElementSize];
+}
+
+export default useResizeObserverElement;
