63ad693db6f89a28dd63be5da6576e969cd3aa9d
njcushing <129661299+njcushing@users.noreply.github.com>
2024-10-31 15:25:27 +0000

    Set style property on root element from value in config file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { useState, useEffect } from "react";
 import { Pages } from "./pages";
+import config from "./appconfig.json";
 
 export function App() {
+    const [rootElement] = useState<HTMLElement>(document.getElementById("root") as HTMLElement);
+
+    // Use applicable app config values
+    useEffect(() => {
+        rootElement.style.maxWidth = config.displayMaxWidth;
+    }, [rootElement]);
+
     return <Pages.PokerHandCalculator />;
 }
