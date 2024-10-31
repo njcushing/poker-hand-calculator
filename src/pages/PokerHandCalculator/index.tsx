766ea650bb8e484fe336d12f3486e799f9d002ea
njcushing <129661299+njcushing@users.noreply.github.com>
2024-10-31 16:33:53 +0000

    Monitor page component size and set layout state accordingly

--- src/pages/PokerHandCalculator/index.tsx
+++ src/pages/PokerHandCalculator/index.tsx
@@ -1,8 +1,19 @@
+import { useState, useEffect, useRef } from "react";
+import useResizeObserverElement from "@/hooks/useResizeObserverElement";
 import styles from "./index.module.css";
 
 export function PokerHandCalculator() {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [containerSize] = useResizeObserverElement({ ref: containerRef });
+    const [layout, setLayout] = useState<"wide" | "thin">("wide");
+
+    useEffect(() => {
+        if (containerSize[0] >= 800) setLayout("wide");
+        else setLayout("thin");
+    }, [containerSize]);
+
     return (
-        <div className={styles["page"]}>
+        <div className={`${styles["page"]} ${styles[`${layout}`]}`} ref={containerRef}>
             <div className={styles["left-panel"]}></div>
             <div className={styles["right-panel"]}></div>
         </div>
