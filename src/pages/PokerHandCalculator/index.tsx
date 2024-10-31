4edfda43404c9e5eb30c5f54f76eaa82bf67880c
njcushing <129661299+njcushing@users.noreply.github.com>
2024-10-31 16:21:05 +0000

    Create layout for left and right panels in wide screen

--- src/pages/PokerHandCalculator/index.tsx
+++ src/pages/PokerHandCalculator/index.tsx
@@ -1,5 +1,10 @@
 import styles from "./index.module.css";
 
 export function PokerHandCalculator() {
-    return <div className={styles["page"]}></div>;
+    return (
+        <div className={styles["page"]}>
+            <div className={styles["left-panel"]}></div>
+            <div className={styles["right-panel"]}></div>
+        </div>
+    );
 }
