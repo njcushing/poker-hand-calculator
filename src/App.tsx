7e56f40e079d90d0c60787ae309608e338e9f5ee
njcushing <129661299+njcushing@users.noreply.github.com>
2024-10-31 15:26:14 +0000

    Set 1% of window inner width and height as global CSS properties

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,23 @@ import { Pages } from "./pages";
 import config from "./appconfig.json";
 
 export function App() {
+    // Calculate 'vw' and 'vh' units
+    useEffect(() => {
+        const setUnits = () => {
+            const vw = window.innerWidth * 0.01;
+            const vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty("--vw", `${vw}px`);
+            document.documentElement.style.setProperty("--vh", `${vh}px`);
+        };
+
+        setUnits();
+        window.addEventListener("resize", setUnits);
+
+        return () => {
+            window.removeEventListener("resize", setUnits);
+        };
+    }, []);
+
     const [rootElement] = useState<HTMLElement>(document.getElementById("root") as HTMLElement);
 
     // Use applicable app config values
