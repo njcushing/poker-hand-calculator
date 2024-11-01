adda8381ea852fddf8fad5397056d44bc4b89c39
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-01 14:47:26 +0000

    Fix 'sortDeck' function and move deckFuncs file into sub-directory

--- src/features/Deck/utils/deckFuncs/index.ts
+++ src/features/Deck/utils/deckFuncs/index.ts
@@ -1,6 +1,8 @@
+const suitOrder = ["Heart", "Club", "Diamond", "Spade"];
+
 export type Card = {
     rank: "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "K" | "A";
-    suit: "Diamond" | "Heart" | "Spade" | "Club";
+    suit: "Heart" | "Club" | "Diamond" | "Spade";
     order: number;
 };
 
@@ -8,19 +10,6 @@ export type Deck = Card[];
 
 export const createDeck = (): Deck => {
     return [
-        { rank: "A", suit: "Diamond", order: 1 },
-        { rank: "2", suit: "Diamond", order: 2 },
-        { rank: "3", suit: "Diamond", order: 3 },
-        { rank: "4", suit: "Diamond", order: 4 },
-        { rank: "5", suit: "Diamond", order: 5 },
-        { rank: "6", suit: "Diamond", order: 6 },
-        { rank: "7", suit: "Diamond", order: 7 },
-        { rank: "8", suit: "Diamond", order: 8 },
-        { rank: "9", suit: "Diamond", order: 9 },
-        { rank: "10", suit: "Diamond", order: 10 },
-        { rank: "J", suit: "Diamond", order: 11 },
-        { rank: "Q", suit: "Diamond", order: 12 },
-        { rank: "K", suit: "Diamond", order: 13 },
         { rank: "A", suit: "Heart", order: 1 },
         { rank: "2", suit: "Heart", order: 2 },
         { rank: "3", suit: "Heart", order: 3 },
@@ -34,19 +23,6 @@ export const createDeck = (): Deck => {
         { rank: "J", suit: "Heart", order: 11 },
         { rank: "Q", suit: "Heart", order: 12 },
         { rank: "K", suit: "Heart", order: 13 },
-        { rank: "A", suit: "Spade", order: 1 },
-        { rank: "2", suit: "Spade", order: 2 },
-        { rank: "3", suit: "Spade", order: 3 },
-        { rank: "4", suit: "Spade", order: 4 },
-        { rank: "5", suit: "Spade", order: 5 },
-        { rank: "6", suit: "Spade", order: 6 },
-        { rank: "7", suit: "Spade", order: 7 },
-        { rank: "8", suit: "Spade", order: 8 },
-        { rank: "9", suit: "Spade", order: 9 },
-        { rank: "10", suit: "Spade", order: 10 },
-        { rank: "J", suit: "Spade", order: 11 },
-        { rank: "Q", suit: "Spade", order: 12 },
-        { rank: "K", suit: "Spade", order: 13 },
         { rank: "A", suit: "Club", order: 1 },
         { rank: "2", suit: "Club", order: 2 },
         { rank: "3", suit: "Club", order: 3 },
@@ -60,6 +36,32 @@ export const createDeck = (): Deck => {
         { rank: "J", suit: "Club", order: 11 },
         { rank: "Q", suit: "Club", order: 12 },
         { rank: "K", suit: "Club", order: 13 },
+        { rank: "A", suit: "Diamond", order: 1 },
+        { rank: "2", suit: "Diamond", order: 2 },
+        { rank: "3", suit: "Diamond", order: 3 },
+        { rank: "4", suit: "Diamond", order: 4 },
+        { rank: "5", suit: "Diamond", order: 5 },
+        { rank: "6", suit: "Diamond", order: 6 },
+        { rank: "7", suit: "Diamond", order: 7 },
+        { rank: "8", suit: "Diamond", order: 8 },
+        { rank: "9", suit: "Diamond", order: 9 },
+        { rank: "10", suit: "Diamond", order: 10 },
+        { rank: "J", suit: "Diamond", order: 11 },
+        { rank: "Q", suit: "Diamond", order: 12 },
+        { rank: "K", suit: "Diamond", order: 13 },
+        { rank: "A", suit: "Spade", order: 1 },
+        { rank: "2", suit: "Spade", order: 2 },
+        { rank: "3", suit: "Spade", order: 3 },
+        { rank: "4", suit: "Spade", order: 4 },
+        { rank: "5", suit: "Spade", order: 5 },
+        { rank: "6", suit: "Spade", order: 6 },
+        { rank: "7", suit: "Spade", order: 7 },
+        { rank: "8", suit: "Spade", order: 8 },
+        { rank: "9", suit: "Spade", order: 9 },
+        { rank: "10", suit: "Spade", order: 10 },
+        { rank: "J", suit: "Spade", order: 11 },
+        { rank: "Q", suit: "Spade", order: 12 },
+        { rank: "K", suit: "Spade", order: 13 },
     ];
 };
 
@@ -76,6 +78,16 @@ export const shuffleDeck = (deck: Deck): Deck => {
             deck[currentIndex],
         ];
     }
+
+    return mutableDeck;
+};
+
+export const sortDeck = (deck: Deck): Deck => {
+    const mutableDeck = deck;
+
+    mutableDeck.sort((a, b) => {
+        return suitOrder.indexOf(a.suit) - suitOrder.indexOf(b.suit) || a.order - b.order;
+    });
 
     return mutableDeck;
 };
