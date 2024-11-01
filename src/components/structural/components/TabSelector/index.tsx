39483315d8391be9fc96bb4f8f4768d457ad0ba4
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-01 11:02:12 +0000

    Display buttons for each tab

--- src/components/structural/components/TabSelector/index.tsx
+++ src/components/structural/components/TabSelector/index.tsx
@@ -1,5 +1,40 @@
+import { useState } from "react";
 import styles from "./index.module.css";
 
-export function TabSelector() {
-    return <div className={styles["tab-selector"]}></div>;
+type Tab = {
+    name?: string;
+    content: JSX.Element | JSX.Element[];
+};
+
+type Tabs = {
+    [key: string]: Tab;
+};
+
+export type TTabSelector = {
+    tabs: Tabs;
+    selectedTabName?: keyof Tabs;
+};
+
+export function TabSelector({ tabs, selectedTabName }: TTabSelector) {
+    const [selectedTab, setSelectedTab] = useState<keyof Tabs>(
+        selectedTabName || Object.keys(tabs)[0],
+    );
+
+    return (
+        <div className={styles["tab-selector"]}>
+            <ul className={styles["tabs"]}>
+                {Object.keys(tabs).map((key) => {
+                    const tab = tabs[key];
+                    return (
+                        <li className={styles["tab"]} key={key}>
+                            <button type="button" className={styles["tab-button"]}>
+                                {tab.name || key}
+                            </button>
+                        </li>
+                    );
+                })}
+            </ul>
+            <div className={styles["tab-content"]}></div>
+        </div>
+    );
 }
