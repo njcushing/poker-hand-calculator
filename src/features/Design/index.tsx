4b2a31fa9e7539422e7aca8f361ade7246f1c361
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-07 10:29:01 +0000

    Increment number of hands when clicking the buttons
    The number of hands is capped between 1 and 9 inclusive.

--- src/features/Design/index.tsx
+++ src/features/Design/index.tsx
@@ -3,7 +3,9 @@ import { PokerHandCalculatorContext } from "@/pages/PokerHandCalculator";
 import styles from "./index.module.css";
 
 export function Design() {
-    const { pokerHandCalculatorState } = useContext(PokerHandCalculatorContext);
+    const { pokerHandCalculatorState, setPokerHandCalculatorStateProperty } = useContext(
+        PokerHandCalculatorContext,
+    );
 
     return (
         <div className={styles["design"]}>
@@ -14,6 +16,10 @@ export function Design() {
                         type="button"
                         className={`${styles["decrease-button"]} material-symbols-sharp`}
                         onClick={(e) => {
+                            setPokerHandCalculatorStateProperty(
+                                "numberOfHands",
+                                Math.max(1, pokerHandCalculatorState.numberOfHands - 1),
+                            );
                             e.currentTarget.blur();
                         }}
                         onMouseLeave={(e) => {
@@ -29,6 +35,10 @@ export function Design() {
                         type="button"
                         className={`${styles["increase-button"]} material-symbols-sharp`}
                         onClick={(e) => {
+                            setPokerHandCalculatorStateProperty(
+                                "numberOfHands",
+                                Math.min(9, pokerHandCalculatorState.numberOfHands + 1),
+                            );
                             e.currentTarget.blur();
                         }}
                         onMouseLeave={(e) => {
