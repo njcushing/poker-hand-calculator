1a85eb39ed4bd8c8f012b2c5cf3521b544161344
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-07 10:06:45 +0000

    Update PokerHandCalculator page component
    It now provides context to all descendant components regarding the state of the application, including a function to mutate that state. This allows me to keep all the relevant information about the current state of the session centralised while switching between tabs - I can simply restore any information using the state.

--- src/pages/PokerHandCalculator/index.tsx
+++ src/pages/PokerHandCalculator/index.tsx
@@ -1,10 +1,38 @@
-import { useState, useEffect, useRef } from "react";
+import { createContext, useState, useEffect, useCallback, useRef, useMemo } from "react";
 import useResizeObserverElement from "@/hooks/useResizeObserverElement";
 import { Structural } from "@/components/structural";
 import { version } from "../../../package.json";
 import styles from "./index.module.css";
 
+export type PokerHandCalculatorState = {
+    numberOfHands: number;
+};
+
+const defaultPokerHandCalculatorState: PokerHandCalculatorState = {
+    numberOfHands: 1,
+};
+
+interface PokerHandCalculatorContext {
+    pokerHandCalculatorState: PokerHandCalculatorState;
+    setPokerHandCalculatorStateProperty: <K extends keyof PokerHandCalculatorState>(
+        property: K,
+        value: PokerHandCalculatorState[K],
+    ) => void;
+}
+
+const defaultPokerHandCalculatorContext: PokerHandCalculatorContext = {
+    pokerHandCalculatorState: defaultPokerHandCalculatorState,
+    setPokerHandCalculatorStateProperty: () => {},
+};
+
+export const PokerHandCalculatorContext = createContext<PokerHandCalculatorContext>(
+    defaultPokerHandCalculatorContext,
+);
+
 export function PokerHandCalculator() {
+    const [pokerHandCalculatorState, setPokerHandCalculatorState] =
+        useState<PokerHandCalculatorState>(defaultPokerHandCalculatorState);
+
     const containerRef = useRef<HTMLDivElement>(null);
     const [containerSize] = useResizeObserverElement({ ref: containerRef });
     const [layout, setLayout] = useState<"wide" | "thin">("wide");
@@ -14,27 +42,52 @@ export function PokerHandCalculator() {
         else setLayout("thin");
     }, [containerSize]);
 
+    const setPokerHandCalculatorStateProperty = useCallback(
+        <K extends keyof PokerHandCalculatorState>(
+            property: K,
+            value: PokerHandCalculatorState[K],
+        ) => {
+            setPokerHandCalculatorState((current) => {
+                const mutableState = { ...current };
+                mutableState[property] = value;
+                return mutableState;
+            });
+        },
+        [],
+    );
+
     return (
-        <div className={`${styles["page"]} ${styles[`${layout}`]}`} ref={containerRef}>
-            <div className={styles["left-panel"]}>
-                <h1 className={styles["title"]}>Poker Hand Calculator</h1>
-                <p className={styles["name"]}>by njcushing</p>
-                <p className={styles["version"]}>{`v${version}`}</p>
-                <Structural.TabSelector
-                    tabs={{
-                        design: { name: "Design", content: <p>Design</p>, position: "left" },
-                        about: { name: "About", content: <p>About</p>, position: "right" },
-                    }}
-                />
+        <PokerHandCalculatorContext.Provider
+            value={useMemo(
+                () => ({ pokerHandCalculatorState, setPokerHandCalculatorStateProperty }),
+                [pokerHandCalculatorState, setPokerHandCalculatorStateProperty],
+            )}
+        >
+            <div className={`${styles["page"]} ${styles[`${layout}`]}`} ref={containerRef}>
+                <div className={styles["left-panel"]}>
+                    <h1 className={styles["title"]}>Poker Hand Calculator</h1>
+                    <p className={styles["name"]}>by njcushing</p>
+                    <p className={styles["version"]}>{`v${version}`}</p>
+                    <Structural.TabSelector
+                        tabs={{
+                            design: { name: "Design", content: <p>Design</p>, position: "left" },
+                            about: { name: "About", content: <p>About</p>, position: "right" },
+                        }}
+                    />
+                </div>
+                <div className={styles["right-panel"]}>
+                    <Structural.TabSelector
+                        tabs={{
+                            simulate: {
+                                name: "Simulate",
+                                content: <p>Simulate</p>,
+                                position: "left",
+                            },
+                            data: { name: "Data", content: <p>Data</p>, position: "left" },
+                        }}
+                    />
+                </div>
             </div>
-            <div className={styles["right-panel"]}>
-                <Structural.TabSelector
-                    tabs={{
-                        simulate: { name: "Simulate", content: <p>Simulate</p>, position: "left" },
-                        data: { name: "Data", content: <p>Data</p>, position: "left" },
-                    }}
-                />
-            </div>
-        </div>
+        </PokerHandCalculatorContext.Provider>
     );
 }
