5f5370b932edad593d6405f11a2438f44a536d8d
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-07 11:31:47 +0000

    Display number of Hand components corresponding to state value

--- src/features/Simulate/index.tsx
+++ src/features/Simulate/index.tsx
@@ -1,5 +1,19 @@
+import { useContext } from "react";
+import { PokerHandCalculatorContext } from "@/pages/PokerHandCalculator";
+import { v4 as uuid } from "uuid";
+import { Hand } from "./components/Hand";
 import styles from "./index.module.css";
 
 export function Simulate() {
-    return <div className={styles["simulate"]}></div>;
+    const { pokerHandCalculatorState } = useContext(PokerHandCalculatorContext);
+
+    return (
+        <div className={styles["simulate"]}>
+            <div className={styles["hands-container"]}>
+                {Array.from(Array(pokerHandCalculatorState.numberOfHands)).map(() => {
+                    return <Hand key={uuid()} />;
+                })}
+            </div>
+        </div>
+    );
 }
