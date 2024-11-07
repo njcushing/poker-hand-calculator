9233c6354c65cbda60d74043b3a5ab88effcc12f
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-07 11:37:06 +0000

    Display two child Card components in the Hand component

--- src/features/Simulate/components/Hand/index.tsx
+++ src/features/Simulate/components/Hand/index.tsx
@@ -1,5 +1,13 @@
+import { Card } from "../Card";
 import styles from "./index.module.css";
 
 export function Hand() {
-    return <div className={styles["hand"]}></div>;
+    return (
+        <div className={styles["hand"]}>
+            <div className={styles["cards"]}>
+                <Card />
+                <Card />
+            </div>
+        </div>
+    );
 }
