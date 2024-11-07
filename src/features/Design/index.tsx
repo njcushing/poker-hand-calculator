7d456f787e586801846da6afedc2be140c12c1f3
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-07 10:26:47 +0000

    Add display for number of hands with decrease and increase buttons

--- src/features/Design/index.tsx
+++ src/features/Design/index.tsx
@@ -1,5 +1,44 @@
+import { useContext } from "react";
+import { PokerHandCalculatorContext } from "@/pages/PokerHandCalculator";
 import styles from "./index.module.css";
 
 export function Design() {
-    return <div className={styles["design"]}></div>;
+    const { pokerHandCalculatorState } = useContext(PokerHandCalculatorContext);
+
+    return (
+        <div className={styles["design"]}>
+            <div className={styles["field-container"]}>
+                <p className={`${styles["field-name"]} truncate-ellipsis`}>Number of Hands:</p>
+                <div className={styles["field-options"]}>
+                    <button
+                        type="button"
+                        className={`${styles["decrease-button"]} material-symbols-sharp`}
+                        onClick={(e) => {
+                            e.currentTarget.blur();
+                        }}
+                        onMouseLeave={(e) => {
+                            e.currentTarget.blur();
+                        }}
+                    >
+                        Remove
+                    </button>
+                    <p className={styles["field-value"]}>
+                        {pokerHandCalculatorState.numberOfHands}
+                    </p>
+                    <button
+                        type="button"
+                        className={`${styles["increase-button"]} material-symbols-sharp`}
+                        onClick={(e) => {
+                            e.currentTarget.blur();
+                        }}
+                        onMouseLeave={(e) => {
+                            e.currentTarget.blur();
+                        }}
+                    >
+                        Add
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
