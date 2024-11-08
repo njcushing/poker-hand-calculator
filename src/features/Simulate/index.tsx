a178510a4eb8f5226cc712b11a83503fae80c323
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-08 10:55:57 +0000

    Prevent rerendering of existing child Hand components on state change

--- src/features/Simulate/index.tsx
+++ src/features/Simulate/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { PokerHandCalculatorContext } from "@/pages/PokerHandCalculator";
 import { v4 as uuid } from "uuid";
 import { Hand } from "./components/Hand";
@@ -7,13 +7,23 @@ import styles from "./index.module.css";
 export function Simulate() {
     const { pokerHandCalculatorState } = useContext(PokerHandCalculatorContext);
 
+    const [hands, setHands] = useState<JSX.Element[]>([]);
+    useEffect(() => {
+        setHands((currentHands) => {
+            const newHands = [...currentHands];
+            while (newHands.length > pokerHandCalculatorState.numberOfHands) {
+                newHands.pop();
+            }
+            while (newHands.length < pokerHandCalculatorState.numberOfHands) {
+                newHands.push(<Hand key={uuid()} />);
+            }
+            return newHands;
+        });
+    }, [pokerHandCalculatorState.numberOfHands]);
+
     return (
         <div className={styles["simulate"]}>
-            <div className={styles["hands-container"]}>
-                {Array.from(Array(pokerHandCalculatorState.numberOfHands)).map(() => {
-                    return <Hand key={uuid()} />;
-                })}
-            </div>
+            <div className={styles["hands-container"]}>{hands}</div>
         </div>
     );
 }
