53e368f80012ec5615361063c9ceb39e1726ca84
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-14 10:17:32 +0000

    Display the cards on the board

--- src/features/Simulate/components/Board/index.tsx
+++ src/features/Simulate/components/Board/index.tsx
@@ -1,5 +1,19 @@
+import { useContext } from "react";
+import { PokerHandCalculatorContext } from "@/pages/PokerHandCalculator";
 import styles from "./index.module.css";
+import { Card } from "../Card";
 
 export function Board() {
-    return <div className={styles["board"]}></div>;
+    const { pokerHandCalculatorState } = useContext(PokerHandCalculatorContext);
+
+    return (
+        <div className={styles["board"]}>
+            <p className={styles["board-name"]}>Board</p>
+            <div className={styles["cards"]}>
+                {pokerHandCalculatorState.board.map((card) => {
+                    return <Card info={card} key={card.order} />;
+                })}
+            </div>
+        </div>
+    );
 }
