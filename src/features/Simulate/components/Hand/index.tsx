97c346e48ce7a501434efa01e426b87fc963c14a
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-15 12:30:57 +0000

    Display each hand's rank

--- src/features/Simulate/components/Hand/index.tsx
+++ src/features/Simulate/components/Hand/index.tsx
@@ -1,4 +1,6 @@
-import { Hand as THandInfo } from "@/features/Deck/utils/deckFuncs";
+import { useContext } from "react";
+import { PokerHandCalculatorContext } from "@/pages/PokerHandCalculator";
+import { calculateHandStrength, Hand as THandInfo } from "@/features/Deck/utils/deckFuncs";
 import { Card } from "../Card";
 import styles from "./index.module.css";
 
@@ -8,6 +10,8 @@ export type THand = {
 };
 
 export function Hand({ info, number }: THand) {
+    const { pokerHandCalculatorState } = useContext(PokerHandCalculatorContext);
+
     return (
         <div className={styles["hand"]}>
             <p className={styles["hand-id"]}>{`Hand ${number}`}</p>
@@ -15,6 +19,9 @@ export function Hand({ info, number }: THand) {
                 <Card info={info[0]} />
                 <Card info={info[1]} />
             </div>
+            <p className={styles["hand-strength"]}>
+                {calculateHandStrength(info, pokerHandCalculatorState.board).rank}
+            </p>
         </div>
     );
 }
