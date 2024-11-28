1c2c94adca79588032279ba4330619dc2e97e489
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-28 12:13:49 +0000

    Display name and buttons for each available card in deck

--- src/features/Simulate/components/CardSelection/index.tsx
+++ src/features/Simulate/components/CardSelection/index.tsx
@@ -1,5 +1,36 @@
+import { useContext } from "react";
+import { PokerHandCalculatorContext } from "@/pages/PokerHandCalculator";
+import { suitSVG } from "../Card/utils/suitSVGs";
 import styles from "./index.module.css";
 
 export function CardSelection() {
-    return <div className={styles["card-selection"]}></div>;
+    const { pokerHandCalculatorState } = useContext(PokerHandCalculatorContext);
+
+    return (
+        <div className={styles["card-selection"]}>
+            <p className={styles["card-selection-name"]}>Available Cards</p>
+            <ul className={styles["card-list"]}>
+                {pokerHandCalculatorState.currentDeck
+                    .sort((a, b) => a.order - b.order)
+                    .map((card) => {
+                        return (
+                            <button
+                                type="button"
+                                className={styles["card"]}
+                                onClick={(e) => {
+                                    e.currentTarget.blur();
+                                }}
+                                onMouseLeave={(e) => {
+                                    e.currentTarget.blur();
+                                }}
+                                key={`card-${card.order}`}
+                            >
+                                <p className={styles["suit"]}>{suitSVG(card.suit)}</p>
+                                <p className={styles["rank"]}>{card.rank}</p>
+                            </button>
+                        );
+                    })}
+            </ul>
+        </div>
+    );
 }
