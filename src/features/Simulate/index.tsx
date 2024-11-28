52df9ae4848f34f080f745f780d1d698ba7e6ddd
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-28 11:41:55 +0000

    Provide 'selectingCard' state and setter in context

--- src/features/Simulate/index.tsx
+++ src/features/Simulate/index.tsx
@@ -1,23 +1,47 @@
-import { useContext } from "react";
+import { createContext, useContext, useState, useMemo, useEffect } from "react";
 import { PokerHandCalculatorContext } from "@/pages/PokerHandCalculator";
 import { v4 as uuid } from "uuid";
 import { Hand } from "./components/Hand";
 import { Board } from "./components/Board";
 import styles from "./index.module.css";
 
+type TSelectingCard = [number, number] | null;
+
+interface SimulateContext {
+    selectingCard: TSelectingCard;
+    setSelectingCard: React.Dispatch<React.SetStateAction<TSelectingCard>>;
+}
+
+const defaultSimulateContext: SimulateContext = {
+    selectingCard: null,
+    setSelectingCard: () => {},
+};
+
+export const SimulateContext = createContext<SimulateContext>(defaultSimulateContext);
+
 export function Simulate() {
     const { pokerHandCalculatorState } = useContext(PokerHandCalculatorContext);
 
+    const [selectingCard, setSelectingCard] = useState<TSelectingCard>(null);
+    useEffect(() => setSelectingCard(null), [pokerHandCalculatorState]);
+
     return (
-        <div className={styles["simulate"]}>
-            <div className={styles["hands-wrapper"]}>
-                <div className={styles["hands-container"]}>
-                    {pokerHandCalculatorState.currentHands.map((hand, i) => {
-                        return <Hand info={hand} number={i + 1} key={`hand-${uuid()}`} />;
-                    })}
+        <SimulateContext.Provider
+            value={useMemo(
+                () => ({ selectingCard, setSelectingCard }),
+                [selectingCard, setSelectingCard],
+            )}
+        >
+            <div className={styles["simulate"]}>
+                <div className={styles["hands-wrapper"]}>
+                    <div className={styles["hands-container"]}>
+                        {pokerHandCalculatorState.currentHands.map((hand, i) => {
+                            return <Hand info={hand} number={i + 1} key={`hand-${uuid()}`} />;
+                        })}
+                    </div>
                 </div>
+                <Board />
             </div>
-            <Board />
-        </div>
+        </SimulateContext.Provider>
     );
 }
