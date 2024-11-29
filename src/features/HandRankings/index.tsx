6ac22bc53697e4ba980e271de12ad11954427645
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-29 13:00:07 +0000

    Add instructions for finding the winner from hands with the same ranking

--- src/features/HandRankings/index.tsx
+++ src/features/HandRankings/index.tsx
@@ -132,7 +132,7 @@ const handRankings: THandRanking[] = [
 export function HandRankings() {
     return (
         <ul className={styles["hand-rankings"]}>
-            <p className={styles["hand-rankings-title"]}>
+            <p className={styles["hand-rankings-intro"]}>
                 The following hand rankings are ordered from strongest to weakest. Highlighted cards
                 are the ones responsible for giving a hand its ranking.
             </p>
@@ -165,6 +165,20 @@ export function HandRankings() {
                     </li>
                 );
             })}
+            <div className={styles["separator"]}></div>
+            <p className={styles["hand-rankings-winner-description"]}>
+                {`In cases where two or more hands have the same ranking, the winner is determined
+                by comparing the highest-ranking card from each hand that forms the ranking for that
+                hand. If these cards are identical in value, the next highest-ranking cards that
+                comprise the ranking for that hand are compared, and so on, until a winner is
+                determined.`}
+            </p>
+            <p className={styles["hand-rankings-winner-description"]}>
+                {`If all cards that comprise the ranking are identical in value, any 'kickers'
+                (the highest-value cards remaining that form the best possible 5-card hand) are
+                compared in the same way. This process continues until a winner is decided or the
+                hands are tied.`}
+            </p>
         </ul>
     );
 }
